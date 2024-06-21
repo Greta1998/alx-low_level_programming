e6f95c2274168b470eac0448bd18d81d32ead129
Greta <tangreta98@gmail.com>
2024-06-20 17:58:09 -0700

    Outputing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,9 @@
+/*
+ * file: 4-puts.c
+ * author: Greta
+ * date: June 21, 2024
+ * descrption: This program outputs using puts
+ * /
 #include <stdio.h>
 int main(void){
 	puts("programming is like buildong a multilingual puzzle");
