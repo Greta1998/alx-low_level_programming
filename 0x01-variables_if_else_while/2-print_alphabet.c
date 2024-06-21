c9044b98701fe2b2b1f002f8a46cf22397c2c070
Greta <tangreta98@gmail.com>
2024-06-21 14:02:19 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Entry point of the program
+ *
+ * Author: Greta
+ *
+ * Description: This program print the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z'){
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
