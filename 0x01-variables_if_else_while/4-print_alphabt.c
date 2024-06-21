864784bf92019e7ec33121fac9639b2f9932250e
Greta <tangreta98@gmail.com>
2024-06-21 16:46:56 -0700

    Lowercase alphabet expect q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point of the program
+ *
+ * Author: Greta
+ *
+ * Description: This program prints lowercase alphabtes expect q and e
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q' || letter == 'e')
+			continue;
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
