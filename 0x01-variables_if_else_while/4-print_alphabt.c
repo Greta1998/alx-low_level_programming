919dcdb257f5627472ef9599de830d889d03590d
Greta <tangreta98@gmail.com>
2024-06-21 16:51:23 -0700

    Lowercase alphabet expect q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,9 +15,8 @@ int main(void)
 
 	while (letter <= 'z')
 	{
-		if (letter == 'q' || letter == 'e')
-			continue;
-		putchar(letter);
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
 		letter++;
 	}
 	putchar('\n');
