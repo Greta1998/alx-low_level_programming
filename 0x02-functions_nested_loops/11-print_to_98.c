5f9ff0813467cbd6f77885e08626511ceebc662a
Greta <tangreta98@gmail.com>
2024-06-30 07:13:11 -0700

    print natural number upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_to_98(int n)- prints all natural numbers from n to 98
+ * @n: the starting number
+ *
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i == 98)
+			{
+				_putchar(i + '0');
+				_putchar('\n');
+			}
+
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+			{
+				_putchar(j + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (j == 98)
+			{
+				_putchar(j + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
