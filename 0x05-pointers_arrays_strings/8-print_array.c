0df4e8f63b58c0beb1feef4cc5fc41bfe40f437a
Greta <tangreta98@gmail.com>
2024-07-01 03:41:15 -0700

    Print array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,8 +9,14 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for(i = 0; i < n; i++)
+ 
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	
+	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
 			printf("%d\n", a[i]);
