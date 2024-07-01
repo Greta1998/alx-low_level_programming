d5a138bade25f659f70c8ba94a1821cf97d98094
Greta <tangreta98@gmail.com>
2024-07-01 01:58:01 -0700

    Reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string(char *s)- reverses a string
+ * @s: the string to be reversed
+ */
+void rev_string(char *s)
+{
+	char c;
+	int i, j, len;
+
+	len = 0;
+	j = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	j = len - 1;
+
+	for (i = 0; i < len /2; i++)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j--] = c;
+	}
+}
