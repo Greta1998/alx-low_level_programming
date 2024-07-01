cdd23ce5870ee4f26eea24b6c11d091a765905c4
Greta <tangreta98@gmail.com>
2024-07-01 04:59:28 -0700

    generate valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+/**
+ * generate_password- generates a random password
+ * @password: points to an array
+ * where the password will be stored
+ * @length: length of the password
+ */
+void generate_password(char *password, int length)
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int size = sizeof(charset) - 1;
+
+	for (int i = 0; i < length; i++)
+	{
+		int key = rand() % size;
+		password[i] = charset[key];
+	}
+	password[length] = '\0';
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0(success)
+ */
+int main (void)
+{
+	char password[PASSWORD_LENGTH + 1];
+
+	srand(time(NULL));
+	generate_password(password, PASSWORD_LENGTH);
+
+	printf("Generated password: %s\n", password);
+
+	return (0);
+}
+	
