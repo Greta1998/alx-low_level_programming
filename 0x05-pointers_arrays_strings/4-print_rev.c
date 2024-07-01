d704b00e838c115a9b5313f7bc0d69d526ea451e
Greta <tangreta98@gmail.com>
2024-07-01 01:44:13 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev(char *s)- prints a string in reverse
+ * @s: the string to be evaluated
+ */
+void print_rev(char *s)
+{
+	int i, j, length;
+
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	length = i;
+
+	for (j = length - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
