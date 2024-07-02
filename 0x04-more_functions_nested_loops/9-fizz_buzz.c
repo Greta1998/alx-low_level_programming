bd9a10967139c02d1f6d36976a47342976057f46
Greta <tangreta98@gmail.com>
2024-07-02 04:22:57 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * multiples of 3: we print Fizz
+ * multiples of 5: we print Buzz
+ * multiples of both 3 and 5: peint FizzBuzz
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i % 3 == 0 && i % 5 != 0)
+			printf(" Fizz");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf(" Buzz");
+		else if (i == 1)
+			printf("%d", i);
+		else
+			printf(" %d", i);
+	}
+	return (0);
+}
