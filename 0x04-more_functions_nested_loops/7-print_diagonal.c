672fe462e98fdd11521c71a13318ee194a167690
Greta <tangreta98@gmail.com>
2024-07-02 03:58:40 -0700

    print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,17 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		int i;
+		int i, j;
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('\\');
+			for (j = 1; j <= i; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ')
+			}
 			_putchar('\n');
 		}
 	}
