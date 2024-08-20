fdfc63bd9349b2d4a00ba34864dedd108e59ba6b
Greta <tangreta98@gmail.com>
2024-08-20 23:33:37 +0300

    file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - compute the ;ength of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: The length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+	while (*str++)
+		++len;
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t b_written = 0;
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, 0_WRONLY | 0_CREATE | 0_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
+	close(fd);
+
+	if (b_written < 0)
+		return (-1);
+	return (1);
+}
